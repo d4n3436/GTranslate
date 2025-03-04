aa4cc080c58de109b47c904fdc81bde6b2f40f71
d4n <dan3436@hotmail.com>
2025-03-04 14:18:55 -0500

    Update default User-Agent header

--- src/GTranslate/Internal/Constants.cs
+++ src/GTranslate/Internal/Constants.cs
@@ -2,5 +2,5 @@
 
 internal static class Constants
 {
-    public const string DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36";
+    public const string DefaultUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36";
 }
